e42a2cd6562dac8bc5d8c0400221585e7e1d8c96
matonsjojc <matevz.podlipnik@gmail.com>
2016-10-14 13:11:30 +0200

    ch. 4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("heeeei paaaartner")
+    return HttpResponse("heiii der pardnr. <a href='/rango/about/'>abut.</a>")
+
+def about(request):
+    return HttpResponse("abut. <a href='/rango/'>index.</a>")
