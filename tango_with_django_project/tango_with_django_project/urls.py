1429d1775bffe8a9ee4cf21fa7d29c7213c97101
matonsjojc <matevz.podlipnik@gmail.com>
2017-02-19 23:15:21 +0100

    ch.11 - do exercises

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-    url('r^accounts/register/$',
-        MyRegistrationView.as_view(),
-            name='registration_register'),
+    url(r'^accounts/register/$',
+        views.MyRegistrationView.as_view(),
+        name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
